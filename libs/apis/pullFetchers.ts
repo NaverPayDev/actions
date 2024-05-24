16ed727cbd139121eadfe624eaed1e2ac81846d1
seunghyun.kevinkim <seunghyun.kevinkim@navercorp.com>
2024-05-24 14:55:25 +0900

    ✨ review list 받아오는 api 작성

--- libs/apis/pullFetchers.ts
+++ libs/apis/pullFetchers.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {RestEndpointMethodTypes} from '@octokit/rest'
 
 import {OctokitRestCommonParamsType} from '$actions/types'
 
 const createPullFetchers = (octokitRestCommonParams: OctokitRestCommonParamsType) => {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const octokit = github.getOctokit(GITHUB_TOKEN)
     const pullApi = github.getOctokit(GITHUB_TOKEN).rest.pulls
     const {
         issue: {number: pull_number},
@@ -23,7 +25,25 @@ const createPullFetchers = (octokitRestCommonParams: OctokitRestCommonParamsType
         return pullInfo
     }
 
-    return {getPullRequestInfo}
+    /**
+     * pull request 의 review list를 가져옵니다.
+     * see) https://docs.github.com/en/rest/pulls/comments?apiVersion=2022-11-28#list-review-comments-on-a-pull-request
+     */
+    const getPullRequestReviews = async () => {
+        const reviewList: RestEndpointMethodTypes['pulls']['listReviews']['response']['data'] = []
+
+        for await (const response of octokit.paginate.iterator(octokit.rest.pulls.listReviews, {
+            ...octokitRestCommonParams,
+            pull_number,
+            per_page: 100,
+        })) {
+            reviewList.push(...response.data)
+        }
+
+        return reviewList
+    }
+
+    return {getPullRequestInfo, getPullRequestReviews}
 }
 
 export default createPullFetchers
